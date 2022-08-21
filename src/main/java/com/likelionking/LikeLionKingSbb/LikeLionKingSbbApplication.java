738ac4e79cb1b4ba6299020a5fbcf14eb79372e6
ahah525 <ahah525@naver.com>
2022-08-21 10:57:03 +0900

    chore : Project Setting

---
+++ src/main/java/com/likelionking/LikeLionKingSbb/LikeLionKingSbbApplication.java
@@ -0,0 +1,13 @@
+package com.likelionking.LikeLionKingSbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLionKingSbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeLionKingSbbApplication.class, args);
+	}
+
+}
