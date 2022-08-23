6b45725b1473ef338b94e5ad1671171762621e3d
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-08-24 03:26:12 +0900

    init

---
+++ sbbstudy/src/test/java/com/likelionking/sbbstudy/SbbstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelionking.sbbstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
