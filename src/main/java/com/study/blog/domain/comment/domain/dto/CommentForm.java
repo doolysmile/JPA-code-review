ef25afe603be77a192719e8418999ffbec09d601
woominsik <9737638@naver.com>
2022-08-24 22:48:28 +0900

    feat : 댓글 생성 기능 구현

---
+++ src/main/java/com/study/blog/domain/comment/domain/dto/CommentForm.java
@@ -0,0 +1,10 @@
+package com.study.blog.domain.comment.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentForm {
+    private String content;
+}
--- src/main/java/com/study/blog/domain/comment/service/CommentService.java
+++ src/main/java/com/study/blog/domain/comment/service/CommentService.java
@@ -1,4 +1,25 @@
 package com.study.blog.domain.comment.service;
 
+import com.study.blog.domain.article.domain.Article;
+import com.study.blog.domain.comment.domain.Comment;
+import com.study.blog.domain.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    public void create(Article article, String content) {
+
+        Comment comment = new Comment();
+        comment.setArticle(article);
+        comment.setCreateDate(LocalDateTime.now());
+        comment.setContent(content);
+
+        commentRepository.save(comment);
+    }
 }
