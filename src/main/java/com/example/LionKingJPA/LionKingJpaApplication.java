8eac3f08ee5bffb43f4a38a31eb71f20237790e4
KCH <doolysmile1@naver.com>
2022-08-24 23:13:44 +0900

    feat : initial commit(Poject Setting)

---
+++ src/main/java/com/example/LionKingJPA/LionKingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LionKingJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionKingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionKingJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LionKingJPA/LionKingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LionKingJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionKingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
