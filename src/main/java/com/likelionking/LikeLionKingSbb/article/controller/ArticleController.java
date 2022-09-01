051b6aa31fae056ced2b068405803cb1d56df7c1
ahah525 <ahah525@naver.com>
2022-09-02 00:30:24 +0900

    faet : 게시글 리스트 paging 적용

--- src/main/java/com/likelionking/LikeLionKingSbb/article/controller/ArticleController.java
+++ src/main/java/com/likelionking/LikeLionKingSbb/article/controller/ArticleController.java
@@ -4,16 +4,13 @@ import com.likelionking.LikeLionKingSbb.article.domain.Article;
 import com.likelionking.LikeLionKingSbb.article.dto.ArticleForm;
 import com.likelionking.LikeLionKingSbb.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/article")
@@ -40,9 +37,9 @@ public class ArticleController {
 
     // 게시글 리스트 조회
     @GetMapping("/list")
-    public String getList(Model model) {
-        List<Article> articleList = articleService.getList();
-        model.addAttribute("articleList", articleList);
+    public String getList(Model model, @RequestParam(defaultValue = "0") int page) {
+        Page<Article> paging = articleService.getList(page);
+        model.addAttribute("paging", paging);
 
         return "article_list";
     }
--- src/main/java/com/likelionking/LikeLionKingSbb/article/service/ArticleService.java
+++ src/main/java/com/likelionking/LikeLionKingSbb/article/service/ArticleService.java
@@ -5,9 +5,11 @@ import com.likelionking.LikeLionKingSbb.article.dto.ArticleForm;
 import com.likelionking.LikeLionKingSbb.article.repository.ArticleRepository;
 import com.likelionking.LikeLionKingSbb.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +17,15 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
 
-    public List<Article> getList() {
-        List<Article> articleList = articleRepository.findAll();
+    public Page<Article> getList(int page) {
+        int size = 10;
+        Sort sort = Sort.by("id").descending();
 
-        return articleList;
+        // 조회할 page, 가져올 아이템 개수, 정렬 방식
+        // Pageable의 구현체 = PageRequest
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return articleRepository.findAll(pageable);
     }
 
     public Article getDetail(Long articleId) {
