6b7076ae907e448748a997aec368f8ea98f8a9b7
KCH <doolysmile1@naver.com>
2022-09-04 17:22:23 +0900

    test : 테스트 코드 추가

--- src/test/java/com/example/LionKingJPA/domain/article/service/ArticleServiceTest.java
+++ src/test/java/com/example/LionKingJPA/domain/article/service/ArticleServiceTest.java
@@ -2,10 +2,14 @@ package com.example.LionKingJPA.domain.article.service;
 
 import com.example.LionKingJPA.domain.article.dto.ArticleDto;
 import com.example.LionKingJPA.domain.article.entity.Article;
+import com.example.LionKingJPA.domain.article.repository.ArticleRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ArticleServiceTest {
     @Autowired
     private ArticleService articleService;
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @Test
     public void 저장(){
@@ -25,6 +32,19 @@ class ArticleServiceTest {
         assertThat(findArticle.getContent().equals(content));
     }
 
+    @Test
+    void creatSamples(){
+        boolean runCheck = true;
+        if(!runCheck){
+            return;
+        }
+        IntStream.rangeClosed(1, 300).forEach(id -> {
+            Article article = new Article();
+            article.setTitle("테스트로 추가된 %d번째 글".formatted(id));
+            article.setContent("테스트로 추가된 %d번 질문의 내용".formatted(id));
 
+            articleRepository.save(article);
+        });
+    }
 
 }
