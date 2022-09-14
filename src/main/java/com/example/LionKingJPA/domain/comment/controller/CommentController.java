960135a9e8d7d526029764ea1b226c5a552167e4
KCH <doolysmile1@naver.com>
2022-09-14 13:01:17 +0900

    feat : 답변 수정 삭제 추가

--- src/main/java/com/example/LionKingJPA/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/LionKingJPA/domain/comment/controller/CommentController.java
@@ -1,17 +1,22 @@
 package com.example.LionKingJPA.domain.comment.controller;
 
+import com.example.LionKingJPA.domain.article.dto.ArticleDto;
 import com.example.LionKingJPA.domain.article.entity.Article;
 import com.example.LionKingJPA.domain.article.service.ArticleService;
 import com.example.LionKingJPA.domain.comment.dto.CommentDto;
+import com.example.LionKingJPA.domain.comment.entity.Comment;
 import com.example.LionKingJPA.domain.comment.service.CommentService;
 import com.example.LionKingJPA.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -39,5 +44,39 @@ public class CommentController {
         commentService.create(commentDto, articleId, userService.getUserByEmail(principal.getName()));
 
         return "redirect:/usr/article/detail/%d".formatted(articleId);
+    }
+    @GetMapping("/modify/{id}")
+    public String modify(CommentDto commentDto, @PathVariable("id") long id, Principal principal){
+        Comment comment = commentService.findById(id);
+        if(!comment.getSiteUser().getEmail().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        commentDto.setContent(commentDto.getContent());
+        return "comment/comment_form";
+    }
+    @PostMapping("/modify/{id}")
+    public String modify(@Valid CommentDto commentDto,  BindingResult bindingResult,  @PathVariable("id") long id, Principal principal){
+        System.out.println(" herehere ");
+        if(bindingResult.hasErrors()){
+            return "comment/comment_form";
+        }
+        Comment findComment = commentService.findById(id);
+        if(!findComment.getSiteUser().getEmail().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentService.modify(id, commentDto);
+
+        return String.format("redirect:/usr/article/detail/%s", findComment.getArticle().getId());
+    }
+
+    @GetMapping("/delete/{id}")
+    public String commentDelete(Principal principal, @PathVariable("id") Long id){
+        Comment findComment = commentService.findById(id);
+        if(!findComment.getSiteUser().getEmail().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        commentService.delete(id);
+        return String.format("redirect:/usr/article/detail/%s", findComment.getArticle().getId());
     }
 }
--- src/main/java/com/example/LionKingJPA/domain/comment/service/CommentService.java
+++ src/main/java/com/example/LionKingJPA/domain/comment/service/CommentService.java
@@ -1,12 +1,17 @@
 package com.example.LionKingJPA.domain.comment.service;
 
+import com.example.LionKingJPA.domain.article.dto.ArticleDto;
 import com.example.LionKingJPA.domain.article.entity.Article;
 import com.example.LionKingJPA.domain.article.repository.ArticleRepository;
 import com.example.LionKingJPA.domain.comment.dto.CommentDto;
+import com.example.LionKingJPA.domain.comment.entity.Comment;
 import com.example.LionKingJPA.domain.comment.repository.CommentRepository;
 import com.example.LionKingJPA.domain.user.entity.SiteUser;
+import com.example.LionKingJPA.global.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +25,22 @@ public class CommentService {
         Article findArticle = articleRepository.findById(articleId).orElse(null);
         return commentRepository.save(CommentDto.toEntity(commentDto, findArticle, siteUser)).getId();
     }
+    public Comment findById(Long id){
+        Optional<Comment> comment = commentRepository.findById(id);
+        if(comment.isPresent()){
+            return comment.get();
+        } else {
+            throw new DataNotFoundException("comment not found");
+        }
+    }
+    public void modify(Long id, CommentDto commentDto){
+        Comment findComment = findById(id);
+        findComment.setContent(findComment.getContent());
 
+        commentRepository.save(findComment);
+    }
+
+    public void delete(Long id){
+        commentRepository.delete(findById(id));
+    }
 }
